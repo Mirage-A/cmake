c190f0206fe1eee2b6123a1d49b85a7f9b18224d
Mirage-A <Zaitsev.EO@phystech.edu>
2019-03-02 19:11:23 +0300

    upd

--- A/preparing.py
+++ A/preparing.py
@@ -1,1 +1,7 @@
-open('index.h', 'tw', encoding='utf-8').close()
+import os
+from pathlib import Path
+
+
+f = open(Path(os.path.abspath(__file__)).parent.__str__() + '/index.h', 'tw', encoding='utf-8')
+f.write('int wtf(int);')
+f.close()
